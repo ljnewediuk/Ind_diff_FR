a21e3460d25ec9761e106f3aafddd09641c80503
Levi <j.newedi@gmail.com>
2020-12-07 16:36:16 -0330

    Update 4-RSS_RMSE.R
    Fix RSS calculations

--- R/4-RSS_RMSE.R
+++ R/4-RSS_RMSE.R
@@ -1,12 +1,26 @@
+#################################################################
+#################################################################
+## 4 - RSS RMSE - COMPARE PERFORMANCE OF GFR AND RANEF MODELS
+#################################################################
+#################################################################
+## Returns:
+## Calculate mean model estimates to use for the RSS plots in
+## the next script
+## 
+## RMSE calculations:
+##    a) Creates RSS vectors for each individual/iteration,
+##       plus a vector from the gfr and ranef with individual
+##       home range characteristics
+##    b) Compares the fit of the gfr and ranef RSS vectors
+##       to the individual RSS using RMSE
 
-library(dplyr)
-library(data.table)
-library(Metrics)
+### Packages ----
+libs <- c('data.table', 'dplyr', 'Metrics')
+lapply(libs, require, character.only = TRUE)
 
+#### 1 - Load data ----
 
-#### 2 - Load data ----
-
-indiv_mods <- readRDS('results/RSF_outputs/id_hab_covariates.rds')
+indiv_mods <- readRDS('results/RSF_outputs/mw_rd_RSFs.rds')
 indiv_mods <- indiv_mods[type=='OOS']
 gfr_mods <- readRDS('results/RSF_outputs/gfr_covariates.rds')
 no_FR_mods <- readRDS('results/RSF_outputs/no_FR_covariates.rds')
@@ -15,83 +29,106 @@ ranef_mods <- readRDS('results/RSF_outputs/ranef_covariates.rds')
 # Remove any indiv_mod iteration with fewer than 150 points
 indiv_mods <- indiv_mods[numb_pts>=150]
 
-####### BOOTSTRAPPING ###########
-
-# For each year, randomly select 6 rows from the allmods data table and allbetas data table with replacement
-# Make comparisons between combinations of betas and model estimates
-
-# Calculate goodness of model fit (i.e. accuracy of model predictions
-# of individual beta coefficients) as the RMSE - ADD THESE TO THE FIGURE
-
-#### 2 - Calculate mean of covariates ----
+#### 2 - Calculate mean of covariates for RSS plots (next script) ----
 
 for(mods in c('gfr', 'no_FR', 'ranef')){
   
   model <- get(paste(mods, 'mods', sep='_'))
   rd <- mean(model[term=='road']$estimate)
   mw <- mean(model[term=='mixedwood']$estimate)
-  conif <- mean(model[term=='coniferous']$estimate)
+  rd_mean <- mean(model[term=='mean_road']$estimate)
+  mw_mean <- mean(model[term=='mean_mixedwood']$estimate)
   rd_rd <- mean(model[term=='road:mean_road']$estimate)
   mw_mw <- mean(model[term=='mixedwood:mean_mixedwood']$estimate)
   rd_mw <- mean(model[term=='road:mean_mixedwood']$estimate)
   mw_rd <- mean(model[term=='mixedwood:mean_road']$estimate)
   
-  assign(paste(mods, 'means', sep='_'), data.table(model=rep(mods, 7), 
-                     covariate=c('rd', 'mw', 'conif', 'rd_rd', 
-                                 'mw_mw', 'rd_mw', 'mw_rd'), beta=c(rd, mw, 
-                                                                   conif, rd_rd, mw_mw, rd_mw, mw_rd)))
+  assign(paste(mods, 'means', sep='_'), data.table(model=rep(mods, 8), 
+                                                   covariate=c('rd', 'mw', 'rd_mean', 'mw_mean', 'rd_rd', 
+                                                               'mw_mw', 'rd_mw', 'mw_rd'), beta=c(rd, mw, rd_mean, mw_mean,
+                                                                                                  rd_rd, mw_mw, rd_mw, mw_rd)))
   
 }
 
 #### 3 - Calculate correlations ----
 
+# Initiate data table
 all_rmse <- data.table()
 
 for(i in unique(indiv_mods$elkyear)){
   
+  # Subset out an individual
   indiv_sub <- indiv_mods[elkyear==i]
   
-  gfr_mw_ests <- c()
-  ranef_mw_ests <- c()
-  
-  indiv_sub_mw <- indiv_sub[term=='mixedwood']
-  
-  for(j in 1:nrow(indiv_sub_mw)){
+  # Loop through all model iterations
+  for(itn in unique(indiv_sub$iteration)){
     
-    mw_gfr <- (gfr_means[covariate=='mw']$beta + gfr_means[covariate=='mw_rd']$beta*log(indiv_sub_mw[j,]$mean_road) + gfr_means[covariate=='mw_mw']$beta*log(indiv_sub_mw[j,]$mean_mixedwood))
-    mw_ranef <- ranef_means[covariate=='mw']$beta
-    
-    gfr_mw_ests <- c(gfr_mw_ests, mw_gfr)
-    ranef_mw_ests <- c(ranef_mw_ests, mw_ranef)
+    # Calculate for both habitats
+    for(hab in c('road', 'mixedwood')){
+      
+      # Set condtions for habitat type
+      if(hab=='mixedwood'){
+        hab1 <- 'mixedwood'
+        hab2 <- 'road'
+        delta_hi <- 1-seq(1,0,length.out=100)
+      } else {
+        hab1 <- 'road'
+        hab2 <- 'mixedwood'
+        delta_hi <- seq(8.5,3.9,length.out=100)
+      }
+      
+      # Set parameters from iteration
+      # Mean of individual home range
+      hi <- indiv_sub[, get(paste("mean", hab1, sep='_'))[term==hab1 & iteration==itn]]
+      hj <- indiv_sub[, get(paste("mean", hab2, sep='_'))[term==hab1 & iteration==itn]]
+      # Individual coefficients 
+      Bi_ind <- indiv_sub[iteration==itn & term==hab1]$estimate
+      Bj_ind <- indiv_sub[iteration==itn & term==hab2]$estimate
+      # ranef coefficients
+      Bi_ranef <- ranef_mods[iteration==itn & term==hab1]$estimate
+      Bj_ranef <- ranef_mods[iteration==itn & term==hab2]$estimate
+      # gfr coefficients
+      Bi_gfr <- gfr_mods[iteration==itn & term==hab1]$estimate
+      Bj_gfr <- gfr_mods[iteration==itn & term==hab2]$estimate
+      Bi_mu <- gfr_mods[term==paste('mean', hab1, sep='_') & iteration==itn]$estimate
+      Bj_mu <- gfr_mods[term==paste('mean', hab2, sep='_') & iteration==itn]$estimate
+      Bij <- gfr_mods[term==paste(hab1, paste('mean', hab2, sep='_'), sep=':') & iteration==itn]$estimate
+      Bji <- gfr_mods[term==paste(hab2, paste('mean', hab1, sep='_'), sep=':') & iteration==itn]$estimate
+      Bii <- gfr_mods[term==paste(hab1, paste('mean', hab1, sep='_'), sep=':') & iteration==itn]$estimate
+      Bjj <- gfr_mods[term==paste(hab2, paste('mean', hab2, sep='_'), sep=':') & iteration==itn]$estimate
+      
+      # Calculate individual RSS
+      if(hab=='mixedwood'){
+        rss_indiv <- (hi-delta_hi)*Bi_ind + log(log(hj)^Bj_ind)
+      } else {
+        rss_indiv <- log((log(hi)/(log(hi)-(log(hi)-delta_hi)))^Bi_ind) + hj*Bj_ind
+      }
+      # Calculate gfr RSS
+      if(hab=='mixedwood'){
+        rss_gfr <- (hi-delta_hi)*(Bi_gfr+Bij*log(hj)+Bii*hi) + log(hj^(Bj_gfr+Bji*hi+Bjj*log(hj))) + hi*Bi_mu + log(hj)^Bj_mu
+      }
+      if(hab=='road'){
+        rss_gfr <- log((log(hi)/(log(hi)-(log(hi)-delta_hi)))^(Bi_gfr+Bij*hj+Bii*log(hi))) + hj*(Bj_gfr+Bji*log(hi)+Bjj*hj) + log(hi)^Bi_mu + hj*Bj_mu
+      }
+      # Calculate ranef RSS
+      if(hab=='mixedwood'){
+        rss_ranef <- (hi-delta_hi)*Bi_ranef + log(log(hj)^Bj_ranef)
+      }
+      if(hab=='road'){
+        rss_ranef <- log((log(hi)/(log(hi)-(log(hi)-delta_hi)))^Bi_ranef) + hj*Bj_ranef
+      }
+      
+      # Calculate RMSE
+      rmse_gfr <- rmse(rss_indiv, rss_gfr)
+      rmse_ranef <- rmse(rss_indiv, rss_ranef)
+      
+      # Compile into data.table
+      rmse_indiv <- data.table(id=i, iteration=itn, habitat=hab, rmse_gfr, rmse_ranef)
+      all_rmse <- rbind(all_rmse, rmse_indiv)
+    }
     
   }
   
-  gfr_rd_ests <- c()
-  ranef_rd_ests <- c()
-  
-  indiv_sub_rd <- indiv_sub[term=='road']
-  
-  for(k in 1:nrow(indiv_sub_rd)){
-    
-    rd_gfr <- (gfr_means[covariate=='rd']$beta + gfr_means[covariate=='rd_mw']$beta*log(indiv_sub_mw[j,]$mean_mixedwood) + gfr_means[covariate=='rd_rd']$beta*log(indiv_sub_mw[j,]$mean_road))
-    rd_ranef <- ranef_means[covariate=='rd']$beta
-    
-    gfr_rd_ests <- c(gfr_rd_ests, rd_gfr)
-    ranef_rd_ests <- c(ranef_rd_ests, rd_ranef)
-    
-  }
-  
-  indiv_mw_ests <- indiv_sub[term=='mixedwood']$estimate
-  indiv_rd_ests <- indiv_sub[term=='road']$estimate
-  
-  rmse_mw_gfr <- rmse(indiv_mw_ests, gfr_mw_ests)
-  rmse_mw_ranef <- rmse(indiv_mw_ests, ranef_mw_ests)
-  rmse_rd_gfr <- rmse(indiv_rd_ests, gfr_rd_ests)
-  rmse_rd_ranef <- rmse(indiv_rd_ests, ranef_rd_ests)
-  
-  rmse_id <- data.table(elkyear=i, ranef_mw=rmse_mw_ranef, gfr_mw=rmse_mw_gfr, ranef_rd=rmse_rd_ranef, gfr_rd=rmse_rd_gfr)
-  
-  all_rmse <- rbind(all_rmse, rmse_id)
 }
 
 #########
@@ -102,4 +139,3 @@ means <- rbind(gfr_means, no_FR_means, ranef_means)
 saveRDS(means, 'results/RSF_outputs/model_means.rds')
 
 saveRDS(all_rmse, 'results/gof/all_rmse.rds')
-
