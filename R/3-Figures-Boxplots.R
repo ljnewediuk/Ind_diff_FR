99b2e11fb3f3a850fc12e002a4f7f755e5a2d079
Levi <j.newedi@gmail.com>
2021-02-04 15:22:28 -0330

    Update 3-Figures-Boxplots.R
    Make boxplots work with new results format

--- R/3-Figures-Boxplots.R
+++ R/3-Figures-Boxplots.R
@@ -16,38 +16,23 @@ lapply(libs, require, character.only = TRUE)
 # Npoints_750<- readRDS('input/exclusion_elk.rds')
 
 ### Load data from RSF outputs folder ----
+all_correlations <- readRDS('results/prediction_outputs/correlations.rds')
 
-# List of all folders
-folder_list <- list.files(path="results/prediction_outputs/")
-
-all_correlations <- data.table()
-
-for (input_folder in folder_list){
-  
-  data <- readRDS(file = paste0("results/prediction_outputs/", paste(input_folder, "correlations.rds", sep = "/")))
-  all_correlations <- rbind(all_correlations, data)
-  
-}
-
-# Optionally exclude elk with fewer than 750 points
-# all_correlations <- all_correlations[!elkyear %in% Npoints_750]
-# Remove any indiv_mod iteration with fewer than 150 points
-all_correlations <- all_correlations[npoints_OOS>=150]
 
 # Calculate difference in all combinations of model comparisons for each iteration
 # Individual & ranef model
-MedianCI(all_correlations$individual-all_correlations$ranef, conf.level=0.95)
+MedianCI(all_correlations$ranef_corr, conf.level=0.95)
 # Individual & GFR model
-MedianCI(all_correlations$individual-all_correlations$gfr, conf.level=0.95)
+MedianCI(all_correlations$gfr_corr, conf.level=0.95)
 # Individual & No FR
-MedianCI(all_correlations$individual-all_correlations$no_FR, conf.level=0.95)
+MedianCI(all_correlations$WS_corr, conf.level=0.95)
 
 # Calculate mean correlations for all individuals
 meanByID <- data.table()
 for (i in unique(all_correlations$elkyear)){
   sub_corrs <- subset(all_correlations, elkyear==i)
   
-  for(model in c('no_FR', 'ranef', 'gfr')){
+  for(model in c('WS_corr', 'ranef_corr', 'gfr_corr')){
     sub_model <- unlist(sub_corrs %>% select(model))
     if(length(sub_model)<6){
       next
@@ -57,8 +42,8 @@ for (i in unique(all_correlations$elkyear)){
     assign(paste(model, 'upper', sep='_'), signif(as.numeric(MeanCI(sub_model, conf.level=0.95)[3]),3))
   }
   
-  summaries <- data.table(elkyear=rep(unlist(i),3), model=c('no_FR', 'ranef', 'gfr'), mean=c(no_FR_mean, ranef_mean, gfr_mean), 
-                          lower=c(no_FR_lower, ranef_lower, gfr_lower), upper=c(no_FR_upper, ranef_upper, gfr_upper))
+  summaries <- data.table(elkyear=rep(unlist(i),3), model=c('WS_corr', 'ranef_corr', 'gfr_corr'), mean=c(WS_corr_mean, ranef_corr_mean, gfr_corr_mean), 
+                          lower=c(WS_corr_lower, ranef_corr_lower, gfr_corr_lower), upper=c(WS_corr_upper, ranef_corr_upper, gfr_corr_upper))
   meanByID <- rbind(meanByID, summaries)
 }
 
@@ -81,11 +66,16 @@ supp_correlations <- merge(bestmodelscores[,1:3], worstmodelscores[,1:3], by='el
 supp_correlations <- supp_correlations[order(elkyear)]
 
 # Make data.table for boxplot
-melt_correlations <- melt(all_correlations, measure.vars=c('individual', 'ranef', 'gfr'))
+melt_correlations <- melt(all_correlations, measure.vars=c('WS_corr', 'ranef_corr', 'gfr_corr'))
+
+# Make data.table for individual correlations
+corr_indivs <- melt_correlations %>% group_by(elkyear, variable) %>% summarize(mean=mean(value, na.rm=T), 
+                                                                               lower=mean(value, na.rm=T)-(sd(value, na.rm=T)/sqrt(length(value))*1.96),
+                                                                               upper=mean(value, na.rm=T)+(sd(value, na.rm=T)/sqrt(length(value))*1.96))
 
 # tiff('figures/fig2.tiff', width = 7, height = 6, units = 'in', res = 300)
 
-ggplot(melt_correlations[variable=='individual' | variable=='no_FR' | variable=='ranef' | variable=='gfr'], aes(x=variable, y=value, col=variable)) + 
+ggplot(melt_correlations, aes(x=variable, y=value, col=variable)) + 
   # Add zero line at median of individual level model
   geom_hline(yintercept=median(melt_correlations[variable=='individual']$value), col='#000000', linetype='dashed') +
   # Add boxplots
@@ -97,11 +87,15 @@ ggplot(melt_correlations[variable=='individual' | variable=='no_FR' | variable==
   # Theme
   theme(panel.background = element_rect(fill='white'), panel.border = element_rect(colour='black', fill=NA), 
         legend.position='none', axis.text = element_text(size=12, colour='black'), axis.title.y = element_text(size=15, colour='black', vjust=2.5)) +
-  ylab(expression(paste('Correlation with individual level model (R'^2, ')'))) + xlab('')
+  ylab(expression(paste('Correlation with individual level model (R'^2, ')'))) + xlab('') +
   # Optionally add points to show worst/best models
-  # + geom_point(data=worstmodelscores, aes(x=worst, y=worst_score), col='black', fill='white', position=position_jitter(width=.1), pch=21, alpha=0.5) +
-  # geom_point(data=bestmodelscores, aes(x=best, y=best_score), col='black', fill='black', position=position_jitter(width=.1), pch=24, alpha=0.5)
- 
+  # geom_point(data=worstmodelscores, aes(x=worst, y=worst_score), col='black', fill='white', position=position_jitter(width=.1), pch=21, alpha=0.5) +
+  # geom_point(data=bestmodelscores, aes(x=best, y=best_score), col='black', fill='black', position=position_jitter(width=.1), pch=24, alpha=0.5)\
+  # Add individuals
+  geom_point(data=corr_indivs, aes(x=variable, y=mean, group=elkyear), position=position_dodge(width=0.5)) +
+  geom_errorbar(data=corr_indivs, aes(x=variable, y=mean, group=elkyear, ymin=lower, ymax=upper), width=2, position=position_dodge(width=0.5)) +
+  geom_smooth(data=corr_indivs, aes(x=variable, y=mean, group=elkyear), se=F, method='lm', colour='#C0C0C075')
+  
 # Save mean correlations by elk year
 saveRDS(meanByID, 'input/mean_correlations.rds')
 
